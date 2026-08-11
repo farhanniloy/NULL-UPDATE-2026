0bbaac75ef914bf36cfdc0e562724390d2126bf6
Farhan Niloy <farhansarkerniloy@gmail.com>
2026-08-12 02:41:47 +0600

    footer: match logo size to navbar; place signature to right on wide screens, below on narrow screens; use '~ nill'

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -19,10 +19,10 @@ const Footer = () => {
             <div className={styles.info}>
                 <Link href="/" className={styles.logo}>null</Link>
                 <div className={styles.quoteRow}>
-                    <span className={styles.signature}>~ nil</span>
                     <p className={styles.text}>
                         what’s good about love is, it’s a very strong desire that realigns the entire tensor of human mind toward a singular madness. the most fundamental incentive (fear) being completely hijacked by its total opposite force, that threshold, among all these mechanistic forces that arise in a human mind, this is the only true madness worth living for! even the gods be envy of...
                     </p>
+                    <span className={styles.signature}>~ nill</span>
                 </div>
             </div>
 
