d27265abb1181b03023b5655e62d0b6ff97d3dc8
Farhan Niloy <techshinobi1@gmail.com>
2026-08-12 12:38:11 +0600

    Add upload date display to article page

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -122,7 +122,7 @@ const SinglePage = async ({ params }) => {
                 </Link>
                 <div className={styles.textContainer}>
                     <span className={styles.date}>
-                        ∅∅∅
+                        {new Date().toISOString().substring(0, 10)} · uploaded {new Date(data.createdAt).toISOString().substring(0, 10)}
                     </span>
                     <h2 className={styles.title}>{data?.title}</h2>
 
